189140e6dd7380bdaf4adb4f055e3982045b8a57
lsg33 <forcesspecial801@gmail.com>
2025-04-03 09:40:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ function parseEnvList(env) {
 }
 
 // Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
-var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
+var checkRateLimit = function () { return false; };
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
